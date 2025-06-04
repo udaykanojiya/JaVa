d94f730ca7ee010f6c39024fe36507d5c7c1dd9f
Uday Kanojiya <udaykanojiya91@gmail.com>
2025-06-04 13:50:38 +0530

    banksynchro

--- bank.java
+++ bank.java
@@ -2,27 +2,28 @@ class WithdrawlThread extends Thread {
     static int totalBalance = 1000;
     int amount;
 
-    public  WithdrawlThread(int amount){
-        this.amount=amount;
+    public WithdrawlThread(int amount) {
+        this.amount = amount;
     }
 
-    public synchronized void run(){
-        if(totalBalance>=amount){
-            System.out.println("paisa apke pass aa jayega" + amount);
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                // TODO: handle exception
+    public synchronized void run() {
+        synchronized (WithdrawlThread.class) {
+            if (totalBalance >= amount) {
+                System.out.println("paisa apke pass aa jayega" + amount);
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+                totalBalance -= amount;
+                ;
+                System.out.println(totalBalance);
+            } else {
+                System.out.println("Gareeb!!!!");
             }
-            totalBalance-=amount;;
-            System.out.println(totalBalance);
-        }
-        else{
-            System.out.println("Gareeb!!!!");
         }
     }
 }
-
 
 public class bank {
     public static void main(String[] args) {
@@ -30,14 +31,13 @@ public class bank {
         WithdrawlThread t2 = new WithdrawlThread(500);
         t1.start();
         // try {
-        //     t1.join();
+        // t1.join();
         // } catch (InterruptedException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
         // }
         t2.start();
 
-        
     }
-    
+
 }
