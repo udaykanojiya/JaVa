da54aca091920597c68e862a2f52763c11751550
Uday Kanojiya <udaykanojiya91@gmail.com>
2025-06-11 10:26:49 +0530

    file

---
+++ CreateFileExample.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CreateFileExample {
+    public static void main(String[] args) {
+        try{
+            File file = new File("new.txt");
+            if(file.createNewFile()){
+                System.out.println("File created: "+ file.getName());
+            }else{
+                System.out.println("File already exist.");
+            }
+
+        }catch(IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
+class WriteToFile{
+    public static void main(String[] args) {
+        try{
+            FileWriter writer = new FileWriter("new.txt");
+            writer.write("Hello, this is a test message written.");
+            writer.close();
+        }catch(IOException e){
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
+}
+
+class ReadFromFile{
+    public static void main(String[] args) {
+        try{
+            File file = new File("example.txt");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+            scanner.close();
+        }catch(FileNotFoundException e){
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
+}
