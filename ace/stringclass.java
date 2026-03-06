665786fc5e8289e0fbfae3c07eff06ba2079bb87
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-06 17:18:45 +0530

    string

---
+++ ace/stringclass.java
@@ -0,0 +1,10 @@
+package ace;
+
+public class stringclass {
+    public static void main(String[] args) {
+        String name = "Uday";
+        String greeting = new String("Hello, " + name + "!");
+        System.out.println(greeting);
+    }
+    
+}
