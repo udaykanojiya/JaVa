fba0ed67d08ba7b7d5a4ca1fadeb6f285f126899
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-08 09:34:28 +0530

    cal

---
+++ ace/calci.java
@@ -0,0 +1,19 @@
+package ace;
+
+public class calci {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;   
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        
+    }
+}
