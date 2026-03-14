c93eeee0ded7202c4b8febf5b0cdfc214505dcdb
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-14 09:45:27 +0530

    datatype

---
+++ javainonevideo/datatypes/Test.java
@@ -0,0 +1,22 @@
+package javainonevideo.datatypes;
+
+public class Test {
+   public static void main(String[] args) {
+        int a =3;
+        float f = 3.14f;
+        double g = 3.141592653589793;
+        short b = 4;
+        long c = 89898989898l;
+        byte d = 55;
+
+        System.out.println("max and min value of int: " + Integer.MAX_VALUE + " and " + Integer.MIN_VALUE);
+        System.out.println("max and min value of short: " + Short.MAX_VALUE + " and " + Short.MIN_VALUE);
+        System.out.println("max and min value of long: " + Long.MAX_VALUE + " and " + Long.MIN_VALUE);
+        System.out.println("max and min value of byte: " + Byte.MAX_VALUE + " and " + Byte.MIN_VALUE);
+        System.out.println("max and min value of float: " + Float.MAX_VALUE + " and " + Float.MIN_VALUE);
+        System.out.println("max and min value of double: " + Double.MAX_VALUE + " and " + Double.MIN_VALUE);
+
+
+   }
+}
+
