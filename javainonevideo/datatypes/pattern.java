038e38635ecaaa68e90d839d7856fe2f1ebdab20
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-16 17:22:25 +0530

    pattern

---
+++ javainonevideo/datatypes/pattern.java
@@ -0,0 +1,13 @@
+package javainonevideo.datatypes;
+
+public class pattern {
+    public static void main(String[] args) {
+        int num = 4;
+      for(int i =0; i<num; i++){
+        for(int j =num; j>0+i ; j--){
+            System.out.print("*");
+        
+        }System.out.println();
+      }
+    }
+}
