0f412e28e551bc0aa4ea484d00bab8d6fa2e2edb
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-18 15:20:55 +0530

    Added encryption and decryption of grade using ternary operator

--- javainonevideo/datatypes/op.java
+++ javainonevideo/datatypes/op.java
@@ -1,11 +1,28 @@
 package javainonevideo.datatypes;
 
+import java.util.Scanner;
+
 public class op {
     public static void main(String[] args) {
 
-        int n=5;
-        boolean x = n%2 == 0 ? true : false;
-        System.out.println(x);
+        // int n=5;
+        // boolean x = n%2 == 0 ? true : false;
+        // System.out.println(x);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the grade: ");
+        int grade = sc.nextInt();
+
+        int encryptedGrade = (grade + 8 )/10;
+        System.out.println("Encrypted grade: " + encryptedGrade);
+
+        int decryptedGrade = (encryptedGrade * 10) - 8;
+        System.out.println("Decrypted grade: " + decryptedGrade);
+
+        sc.close();
+
         
     }
+
 }
