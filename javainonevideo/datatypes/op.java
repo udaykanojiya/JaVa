2acfdadc58f54eca156d840bc2338b28e4f6837a
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-18 15:01:29 +0530

    op

---
+++ javainonevideo/datatypes/op.java
@@ -0,0 +1,11 @@
+package javainonevideo.datatypes;
+
+public class op {
+    public static void main(String[] args) {
+
+        int n=5;
+        boolean x = n%2 == 0 ? true : false;
+        System.out.println(x);
+        
+    }
+}
