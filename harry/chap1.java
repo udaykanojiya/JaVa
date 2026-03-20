1d5d32db9a7df4abd19238b75b7321f40727ae4a
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-20 20:17:37 +0530

    first

--- harry/chap1.java
+++ harry/chap1.java
@@ -3,8 +3,10 @@ package harry;
 public class chap1 {
     public static void main(String[] args) {
 
-        int a = 10, b=11, c=12;
+        float a = 100, b=11, c=72;
         System.out.println(a+b+c);
+
+        System.out.println("CGPA: "+ (a+b+c)*10/300);
         
     }
 }
