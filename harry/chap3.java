d517f6d894aa2d9d2cb91a2157667b7a95afe01d
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-21 15:59:54 +0530

    hi

---
+++ harry/chap3.java
@@ -0,0 +1,9 @@
+package harry;
+
+public class chap3{
+    public static void main(String[] args) {
+        String a = "UdAy";
+        String b = a.toLowerCase();
+        System.out.println(a + " " + b);
+    }
+}
