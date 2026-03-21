bf68aef9b2693e4bf05f04e5e773d3feec1c14e5
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-21 10:28:58 +0530

    chap2

--- harry/chap2.java
+++ harry/chap2.java
@@ -1,8 +1,25 @@
 package harry;
+import java.util.Scanner;
 
 public class chap2 {
     public static void main(String[] args){
         float a = 7.0f/4*9.0f/2;
         System.out.println(a);
+
+        
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a no: ");
+    int b = sc.nextInt();
+    int c = 100;
+    if(b>c){
+        System.out.println("User entered a greater no.");
     }
+    else{
+        System.out.println("User entered a smaller no.");
+    }
+
+    
+
+    }
+
 }
