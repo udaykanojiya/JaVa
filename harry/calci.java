80f0bee5e592a1ff5812fcff55021b1044255a21
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-22 20:55:20 +0530

    good

---
+++ harry/calci.java
@@ -0,0 +1,14 @@
+package harry;
+import java.util.*;
+
+public class calci{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 no: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Choose operator: \n+ - * /");
+        String c = sc.nextLine();
+
+    }
+}
