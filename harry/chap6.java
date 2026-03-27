2ce0e11cd772de6bef17ceedb377becaca0575dc
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-27 16:11:34 +0530

    1

--- harry/chap6.java
+++ harry/chap6.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 public class chap6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int rows = sc.nextInt();
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        // System.out.println("Enter the number of rows:");
+        // int rows = sc.nextInt();
+        // for (int i = 1; i <= rows; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        float[] marks = {1,2,3,4,5};
+            for (float element : marks) {
+                System.out.println(element);
             }
-            System.out.println();
-        }
+
         sc.close();
     }
 }
