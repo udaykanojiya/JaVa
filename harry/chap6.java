60399bcb3f9ce66a62921ea8f8c204804670aab8
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-28 12:57:04 +0530

    6

--- harry/chap6.java
+++ harry/chap6.java
@@ -62,10 +62,14 @@ public class chap6 {
             // for(int element : a){
             //     System.out.print(element + " ");    }
 
-            int max = 0;
+            int max = a[0];
+            int min = a[0];
             for(int i=1;i<a.length;i++){
                 if(a[i]> max){
                     max = a[i];
+                }
+                if(a[i]< min){
+                    min = a[i];
                 }
             }
             System.out.println("Maximum element: " + max);
