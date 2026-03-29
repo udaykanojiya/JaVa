6d426444c7a743cdf25e8ece2175c9493066379b
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-03-29 10:16:04 +0530

    3

--- harry/class8.java
+++ harry/class8.java
@@ -4,12 +4,16 @@ import java.util.*;
 public class class8 {
 public static void main(String[] args){
 
-    Employee uday = new Employee("Uday", 50000);
-    uday.getName();
-    uday.getSalary();   
-    uday.setName("Udayan");
-    uday.getName();
+    // Employee uday = new Employee("Uday", 50000);
+    // uday.getName();
+    // uday.getSalary();   
+    // uday.setName("Udayan");
+    // uday.getName();
 
+    CellPhone oneplus = new CellPhone();
+    oneplus.ringing();
+    oneplus.vibrating();
+    oneplus.callFriend();
     
   
 
@@ -29,8 +33,21 @@ class Employee{
     void getName(){
         System.out.println("Name is " + name);
     }
-    void setName(String Name){
-        name = Name;
+    void setName(String name){
+        this.name = name;
     }
 
 }
+
+class CellPhone{
+    void ringing(){
+        System.out.println("The phone is ringing...");
+    }
+
+    void vibrating(){
+        System.out.println("The phone is vibrating...");}
+
+        void callFriend(){
+            System.out.println("Calling Udayan...");
+        }
+    }
