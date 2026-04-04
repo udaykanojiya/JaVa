ed8a29a47cda23c6dfc194acbec421858fce8e07
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-04-04 09:09:34 +0530

    pr

--- ace/prac.java
+++ ace/prac.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class prac {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the number of rows:");
-        // int rows = sc.nextInt();
+        System.out.println("Enter the number of rows:");
+        int rows = sc.nextInt();
         System.out.println("Enter the pattern:");
         String pattern = sc.nextLine();
         System.out.println("Pattern: " + pattern);
-        // for (int i = 1; i <= rows; i++) {
-        //     for (int j = 1; j <= i; j++) {
-        //         System.out.print(pattern + " ");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(pattern + " ");
+            }
+            System.out.println();
+        }
     }
     
 }
