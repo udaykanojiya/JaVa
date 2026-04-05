6e7fc845589730bb472fdf529edd2551bc69424c
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-04-05 13:12:50 +0530

    error

--- harry/chap6.java
+++ harry/chap6.java
@@ -75,13 +75,13 @@ public class chap6 {
             System.out.println("Maximum element: " + max);
             System.out.println("Minimum element: " + min);
 
-            // chap6 m = new chap6();
-            // m.first(1,2,3,3);
-                // System.out.println(first(5));
+            chap6 m = new chap6();
+            m.first(1,2,3,3);
+                System.out.println(first(5));
 
             
-            // pattern(7);
-                // System.out.println(nat(5));
+            pattern(7);
+                System.out.println(nat(5));
 
           sc.close();
         //   System.out.println(fib(5));
