53373bc6dc639cf06d0d412ddff5e10bb83c5683
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-04-28 10:08:12 +0530

    add

--- harry/class8.java
+++ harry/class8.java
@@ -3,11 +3,11 @@ package harry;
 public class class8 {
 public static void main(String[] args){
 
-    // Employee uday = new Employee("Uday", 50000);
-    // uday.getName();
-    // uday.getSalary();   
-    // uday.setName("Udayan");
-    // uday.getName();
+    Employee uday = new Employee("Uday", 50000);
+    uday.getName();
+    uday.getSalary();   
+    uday.setName("Udayan");
+    uday.getName();
 
     CellPhone oneplus = new CellPhone();
     oneplus.ringing();
