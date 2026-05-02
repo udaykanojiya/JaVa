8c8cb0425e7656cb0ba5c62ae86bb0c9a9024878
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-05-02 10:08:13 +0530

    rem

--- harry/chap3.java
+++ harry/chap3.java
@@ -2,9 +2,9 @@ package harry;
 
 public class chap3{
     public static void main(String[] args) {
-        String a = "UdAy";
-        String b = a.toLowerCase();
-        System.out.println(a + " " + b);
+        // String a = "UdAy";
+        // String b = a.toLowerCase();
+        // System.out.println(a + " " + b);
 
         String c = "Uday is a good boy";
         String d = c.replace(" ", "_");
