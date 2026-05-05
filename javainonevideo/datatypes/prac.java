5e2c5cc815cd57e7b41408eb57737804aeac4244
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-05-05 09:46:14 +0530

    rem

--- javainonevideo/datatypes/prac.java
+++ javainonevideo/datatypes/prac.java
@@ -25,8 +25,8 @@ public class prac {
 
         //Question 3 type of input
         // System.out.print("Enter a number : ");
-        int a = 12 / 5 * 5/2;
-        System.out.println(a);
+        // int a = 12 / 5 * 5/2;
+        // System.out.println(a);
         // if(sc.hasNextInt())
 
 
