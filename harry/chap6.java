35488a008bbdcb0604b8ce3c162d5bff158ae881
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-05-27 13:38:46 +0530

    add

--- harry/chap6.java
+++ harry/chap6.java
@@ -32,9 +32,9 @@ public class chap6 {
 
         //     System.out.println("Average of marks: " + (sum/50)*10);
 
-        // int[][]   a,b;
-        // a = new int[][]{{1,2,3},{4,5,6}};
-        // b = new int[][]{{7,8,9},{10,11,12}};
+        int[][]   a,b;
+        a = new int[][]{{1,2,3},{4,5,6}};
+        b = new int[][]{{7,8,9},{10,11,12}};
 
        
         // for( int i=0;i<a.length;i++){
