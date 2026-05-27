cd15caf388bf74f8d5f1fdaeb337cd65c6697c1d
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-05-27 13:39:21 +0530

    rem

--- harry/chap6.java
+++ harry/chap6.java
@@ -44,12 +44,12 @@ public class chap6 {
         //     System.out.println();
         // }
         
-        int[] a = {1,2,3,4,5};
+        // int[] a = {1,2,3,4,5};
 
-            for(int i = 0; i < a.length/2; i++){
-                int temp = a[i];
-                a[i] = a[a.length - 1 - i];
-                a[a.length - 1 - i] = temp;}
+        //     for(int i = 0; i < a.length/2; i++){
+        //         int temp = a[i];
+        //         a[i] = a[a.length - 1 - i];
+        //         a[a.length - 1 - i] = temp;}
 
             for(int element : a){
                 System.out.print(element + " ");    }
