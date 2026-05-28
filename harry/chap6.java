dbc7c5f6b08e44fb61964a878fbf24c386fd1b16
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-05-28 18:10:07 +0530

    add

--- harry/chap6.java
+++ harry/chap6.java
@@ -44,7 +44,7 @@ public class chap6 {
         //     System.out.println();
         // }
         
-        // int[] a = {1,2,3,4,5};
+        int[] a = {1,2,3,4,5};
 
         //     for(int i = 0; i < a.length/2; i++){
         //         int temp = a[i];
