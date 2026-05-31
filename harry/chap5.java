07fb4d6de00ccf39e0fc33e113dcee0ab35762b3
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-05-31 17:01:12 +0530

    rem

--- harry/chap5.java
+++ harry/chap5.java
@@ -2,12 +2,12 @@ package harry;
 
 public class chap5 {
     public static void main(String[] args) {
-        int n=4;
-        for(int i= 1; i<=n;i++){
-            for(int j=n-i;j>=0;j--){
-                System.out.print("*");
-            }
-            System.out.println("");
+        // int n=4;
+        // for(int i= 1; i<=n;i++){
+        //     for(int j=n-i;j>=0;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
 
         // int sum=0;
         // int i=1;
