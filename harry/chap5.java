178b8e6698c3f3d4db22db30c040dba8902d1073
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-06 08:54:03 +0530

    add

--- harry/chap5.java
+++ harry/chap5.java
@@ -10,13 +10,13 @@ public class chap5 {
             System.out.println("");
 
         int sum=0;
-        // int i=1;
-        // int n=3;
-        // while(i<=n){
-        //     sum=sum+2*i;
-        //     i++;
-        // }
-        // System.out.println(sum);
+        int i=1;
+        int n=3;
+        while(i<=n){
+            sum=sum+2*i;
+            i++;
+        }
+        System.out.println(sum);
 
         
         
