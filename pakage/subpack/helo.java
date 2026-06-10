0816ea471c1fc735b18b84537911de7c16de6ad1
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-10 11:19:36 +0530

    .

--- pakage/subpack/helo.java
+++ pakage/subpack/helo.java
@@ -3,10 +3,10 @@
 public class helo {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
-        char a = 'A';
-        System.out.println(a);
-        System.out.println((char)10 +"end");
+        // System.out.println("Hello World");
+        // char a = 'A';
+        // System.out.println(a);
+        // System.out.println((char)10 +"end");
     }
 
 }
