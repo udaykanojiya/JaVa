d2f16d0bedad49afc6759a42ad136b057c152566
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-11 09:59:48 +0530

    .

--- pakage/subpack/helo.java
+++ pakage/subpack/helo.java
@@ -3,10 +3,12 @@
 public class helo {
 
     public static void main(String[] args){
-        // System.out.println("Hello World");
-        // char a = 'A';
-        // System.out.println(a);
-        // System.out.println((char)10 +"end");
+        System.out.println("Hello World");
+        char a = 'A';
+        System.out.println(a);
+        System.out.println((char)10 +"end");
     }
 
 }
+// Remove-Item -Force .git/index.lock
+
