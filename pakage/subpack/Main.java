898bc18c5ad80f889fd9446e138412c60b2baae5
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-11 10:00:37 +0530

    .

--- pakage/subpack/Main.java
+++ pakage/subpack/Main.java
@@ -4,10 +4,10 @@ public class Main{
     public static void main(String[] args){
         // System.out.println("Good");
         
-        System.out.println("Ranges");
-        short a = 3;
-        System.out.println("Range = " + Short.MIN_VALUE);
-        String text = "Uday is a Good.";
+        // System.out.println("Ranges");
+        // short a = 3;
+        // System.out.println("Range = " + Short.MIN_VALUE);
+        // String text = "Uday is a Good.";
         System.out.println(text.contains("Uday"));
         float c = 10/3.0f                                                           ;
         System.out.println(c);
