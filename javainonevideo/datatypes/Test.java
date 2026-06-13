86c246b0d70bad1ec2d63797143e431216d99bdd
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-13 11:51:05 +0530

    rem

--- javainonevideo/datatypes/Test.java
+++ javainonevideo/datatypes/Test.java
@@ -9,12 +9,12 @@ public class Test {
         long c = 89898989898l;
         byte d = 55;
 
-        System.out.println("max and min value of int: " + Integer.MAX_VALUE + " and " + Integer.MIN_VALUE);
-        System.out.println("max and min value of short: " + Short.MAX_VALUE + " and " + Short.MIN_VALUE);
-        System.out.println("max and min value of long: " + Long.MAX_VALUE + " and " + Long.MIN_VALUE);
-        System.out.println("max and min value of byte: " + Byte.MAX_VALUE + " and " + Byte.MIN_VALUE);
-        System.out.println("max and min value of float: " + Float.MAX_VALUE + " and " + Float.MIN_VALUE);
-        System.out.println("max and min value of double: " + Double.MAX_VALUE + " and " + Double.MIN_VALUE);
+      //   System.out.println("max and min value of int: " + Integer.MAX_VALUE + " and " + Integer.MIN_VALUE);
+      //   System.out.println("max and min value of short: " + Short.MAX_VALUE + " and " + Short.MIN_VALUE);
+      //   System.out.println("max and min value of long: " + Long.MAX_VALUE + " and " + Long.MIN_VALUE);
+      //   System.out.println("max and min value of byte: " + Byte.MAX_VALUE + " and " + Byte.MIN_VALUE);
+      //   System.out.println("max and min value of float: " + Float.MAX_VALUE + " and " + Float.MIN_VALUE);
+      //   System.out.println("max and min value of double: " + Double.MAX_VALUE + " and " + Double.MIN_VALUE);
 
 
    }
