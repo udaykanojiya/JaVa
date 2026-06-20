abfb2753785ae1560f49ad80879735d5a20624f3
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-20 09:43:02 +0530

    add

--- javainonevideo/datatypes/str.java
+++ javainonevideo/datatypes/str.java
@@ -15,9 +15,9 @@ public class str {
         System.out.println(name.charAt(5));
         System.out.println(name.indexOf("in"));
         System.out.println(name.lastIndexOf("in"));
-        System.out.println(name.replace("in", "on"));
-        System.out.println(name.substring(0, 4));
-        name = name.toUpperCase();
-        System.out.println(name);
+        // System.out.println(name.replace("in", "on"));
+        // System.out.println(name.substring(0, 4));
+        // name = name.toUpperCase();
+        // System.out.println(name);
     }
 }
