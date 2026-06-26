f839f2013cb97e6b329f59c624556557e666071e
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-26 14:23:33 +0530

    REM

--- javainonevideo/datatypes/str.java
+++ javainonevideo/datatypes/str.java
@@ -4,11 +4,11 @@ public class str {
     public static void main(String[] args) {
         String name = "java in one in video in";
         System.out.println(name);
-        System.out.println(name.length());
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
-        System.out.println(name.contains("in"));
-        System.out.println(name.startsWith("java"));
+        // System.out.println(name.length());
+        // System.out.println(name.toUpperCase());
+        // System.out.println(name.toLowerCase());
+        // System.out.println(name.contains("in"));
+        // System.out.println(name.startsWith("java"));
         System.out.println(name.endsWith("in"));
         System.out.println(name.equals("java in one in video in"));
         System.out.println(name.equalsIgnoreCase("JAVA IN ONE IN VIDEO IN"));
