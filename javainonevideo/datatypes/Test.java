84436366de837793471b0af8a9d0c9a9b5bad0a1
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-30 08:30:12 +0530

    REM

--- javainonevideo/datatypes/Test.java
+++ javainonevideo/datatypes/Test.java
@@ -5,9 +5,9 @@ public class Test {
         int a =3;
         float f = 3.14f;
         double g = 3.141592653589793;
-        short b = 4;
-        long c = 89898989898l;
-        byte d = 55;
+      //   short b = 4;
+      //   long c = 89898989898l;
+      //   byte d = 55;
 
         System.out.println("max and min value of int: " + Integer.MAX_VALUE + " and " + Integer.MIN_VALUE);
         System.out.println("max and min value of short: " + Short.MAX_VALUE + " and " + Short.MIN_VALUE);
