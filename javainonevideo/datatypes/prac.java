727dc4f2854fef62770f4e97baf95bb14db55152
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-07-06 13:44:13 +0530

    add

--- javainonevideo/datatypes/prac.java
+++ javainonevideo/datatypes/prac.java
@@ -19,9 +19,9 @@ public class prac {
         System.out.println("Enter makrs in three subjects: ");
         int m1 = sc.nextInt();
         int m2 = sc.nextInt();
-        int m3 = sc.nextInt();
-        float total = m1 + m2 + m3;
-        System.out.println("Name: " + name);
+        // int m3 = sc.nextInt();
+        // float total = m1 + m2 + m3;
+        // System.out.println("Name: " + name);
         System.out.println("CGPA: " + total/30);
 
         //Question 3 type of input
