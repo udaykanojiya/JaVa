edc6ad62f3937b81ad631c96aba78e7fb728936a
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-07-15 20:17:59 +0530

    j

--- javainonevideo/datatypes/result.java
+++ javainonevideo/datatypes/result.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class result{
     public static void main(String[] args) {
-      // Scanner sc = new Scanner(System.in);
-      // System.out.print("Enter your marks: ");
-      // String marks = sc.nextLine().toUpperCase();
-      // System.out.println("Your marks are: " + marks);
-      // sc.close();
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter your marks: ");
+      String marks = sc.nextLine().toUpperCase();
+      System.out.println("Your marks are: " + marks);
+      sc.close();
     }
 }
