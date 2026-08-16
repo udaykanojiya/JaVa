210bfaf0dbdcafb7250d365309ce5d5b1f4e69e6
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-08-16 07:26:40 +0530

    j

--- javainonevideo/datatypes/prac.java
+++ javainonevideo/datatypes/prac.java
@@ -24,9 +24,9 @@ public class prac {
         // System.out.println("Name: " + name);
         // System.out.println("CGPA: " + total/30);
 
-        Question 3 type of input
-        System.out.print("Enter a number : ");
-        int a = 12 / 5 * 5/2;
+        // Question 3 type of input
+        // System.out.print("Enter a number : ");
+        // int a = 12 / 5 * 5/2;
         System.out.println(a);
         if(sc.hasNextInt())
         if(sc.hasNextInt()){
