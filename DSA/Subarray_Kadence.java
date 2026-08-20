0eb8df9e3688ec0b8710ea818a9f0fb1f0cb71d3
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-08-20 11:43:24 +0530

    j

--- DSA/Subarray_Kadence.java
+++ DSA/Subarray_Kadence.java
@@ -7,11 +7,11 @@ class Kadence {
         int sum =0;
         for(int i=0 ; i< len;i++){
             
-            if(sum<0){
-                sum = 0;
-            }
-            sum = sum + nums[i];
-            maxi = Math.max(maxi,sum);
+            // if(sum<0){
+            //     sum = 0;
+            // }
+            // sum = sum + nums[i];
+            // maxi = Math.max(maxi,sum);
             // for(int j=i ;j<nums.length; j++){
             //     sum = sum + nums[j];
             //     if(sum>maxi){
